b2d08f6416d7538980ca71b87da345944e0c5d04
karn-ake <karn-ake.r@n2nconnect.com>
2021-11-15 16:25:04 +0700

    add mongodb

--- repository/mongorepository.go
+++ repository/mongorepository.go
@@ -8,6 +8,11 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+type Client struct {
+	logFile string
+	clientName string
+}
+
 type mongoRepository struct {
 	db *mongo.Client
 }
@@ -23,4 +28,18 @@ func (m mongoRepository) FindAll() ([]string, error) {
 	fmt.Println("Connection to MongoDB alive")
 
 	return nil, nil
+}
+
+func (m mongoRepository) Update() {
+	docs := []interface{}{
+		bson.D{{"logFile", "My Brilliant Friend"}, {"clientName", "Elena Ferrante"}},
+		bson.D{{"logFile", "Lucy"}, {"clientName", "Jamaica Kincaid"}},
+		bson.D{{"logFile", "Cat's Cradle"}, {"clientName", "Kurt Vonnegut Jr."}},
+	}
+	coll := m.db.Database("monitor").Collection("log")
+	result, err := coll.InsertMany(context.TODO(),docs)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
--- server.go
+++ server.go
@@ -6,7 +6,6 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
